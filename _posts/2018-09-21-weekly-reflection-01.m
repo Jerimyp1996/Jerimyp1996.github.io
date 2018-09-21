44e60a18ceb1194a3d90d4cf4d9e50a8a0e8f6c2
Jerimyp1996 <43242785+Jerimyp1996@users.noreply.github.com>
2018-09-21 09:48:28 -0400

    Create 2018-09-21-weekly-reflection-01.m

---
+++ _posts/2018-09-21-weekly-reflection-01.m
@@ -0,0 +1,22 @@
+---
+layout: post
+title: Jerimy Perez's Weekly Reflection 01
+date: 2018-09-21
+---
+1. Describe your impressions of the class so far.
+ My impressions of the class so far is that it is a very orginized and interesting class. Creating my own website was really fun and
+ exciting. It makes me feel accomplished even though this is just the beggining of programming. The way we take notes too is very
+ interesting and different but its a very helpful and orginized way of taking notes. I like it. Overall, I am very excited to
+ learn more about Computer Science.
+ 
+2. Describe what you have learned.
+ I have learned how to create diffferent files and directories on Github. I also have learned how to creat a Github Website as well as
+ learned about different markup "languages" like Markdown. In Github, if you want to create a new file in a certain dirrectory you'd have
+ to put "the name of the directory" then a slash and then what you want to name the file. For example, for this post I did _posts/"name of
+ the file". I'm pretty sure I got this down.
+ 
+ 3. Describe your biggest challenge.
+  My biggest challenge was getting the link to my first post to work. I accidently forgot to change the date of the file so when I tried to
+  type the file name into the URL (with the wrong date) it didn't work. I didnt know what was wrong until I saw the file and the date was
+  wrong. The file then didnt let me edit it after a while. When I finally fixed the date to the file, I typed in the correct URL but it
+  still didn't work. I hadn't realized that it takes a while for the post to go up... Eventually my first post was up.
